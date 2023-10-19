1160c42dba96ddd6b7278622e9ef3b9ccdf56782
NicolasMura <contact@nicolasmura.fr>
2023-10-19 11:42:44 +0200

    Init

---
+++ apps/frontend/src/app/task-list/task-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tasks-list-task-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-list.component.html',
+})
+export class TaskListComponent {}
