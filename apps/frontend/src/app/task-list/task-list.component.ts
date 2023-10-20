2d52beebed5cc18a991af37e333812ac3729a21d
NicolasMura <contact@nicolasmura.fr>
2023-10-20 11:08:09 +0200

    feat(tasks): add button to create a new task

--- apps/frontend/src/app/task-list/task-list.component.ts
+++ apps/frontend/src/app/task-list/task-list.component.ts
@@ -1,22 +1,99 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TaskService } from '@ses/api-sdk';
 import { TaskComponent } from '@ses/task';
-import { tap } from 'rxjs';
+
+import { BehaviorSubject, catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'ses-task-list',
   standalone: true,
-  imports: [CommonModule, TaskComponent],
+  imports: [
+    CommonModule,
+    TaskComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './task-list.component.html',
 })
 export class TaskListComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private searchInputRef!: ElementRef;
+
+  @ViewChild('searchInputRef', { static: false }) private set input(
+    searchInputRef: ElementRef,
+  ) {
+    this.searchInputRef = searchInputRef;
+  }
+
   taskService = inject(TaskService);
+  isLoading$ = new BehaviorSubject(false);
+  taskForm = this.formBuilder.group({
+    description: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
+    this.isLoading$.next(true);
     this.taskService
       .fetchTasksFromApi()
-      .pipe(tap((tasks) => this.taskService.setTasks(tasks)))
+      .pipe(
+        tap((tasks) => this.taskService.setTasks(tasks)),
+        catchError(() => {
+          return of([]);
+        }),
+        tap(() => this.isLoading$.next(false)),
+      )
       .subscribe();
+  }
+
+  /**
+   * Scan for CTRL / CMD + F events
+   */
+  @HostListener('window:keydown', ['$event'])
+  onKeyPress($event: KeyboardEvent): void {
+    if (($event.ctrlKey || $event.metaKey) && $event.code === 'KeyF') {
+      $event.preventDefault(); // to prevent the browser from opening its own default search box
+      this.searchInputRef.nativeElement.focus();
+    }
+  }
+
+  addTask(): void {
+    const description = this.taskForm.get('description')?.value;
+
+    if (description) {
+      this.taskService.setTasks([
+        {
+          id: this.taskService.getTasks()[0]?.id + 1 || 1,
+          description,
+          completed: false,
+        },
+        ...this.taskService.getTasks(),
+      ]);
+      this.taskForm.reset();
+      this.taskForm.markAsPristine();
+    }
   }
 }
--- libs/api-sdk/src/api/task.service.ts
+++ libs/api-sdk/src/api/task.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, delay, of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  delay,
+  first,
+  of,
+} from 'rxjs';
 import { ITask } from '../model/task';
 
 const INITIAL_TASKS: ITask[] = [
-  { id: 1, description: 'Task 1', completed: false },
-  { id: 2, description: 'Task 2', completed: true },
   { id: 3, description: 'Task 3', completed: false },
+  { id: 2, description: 'Task 2', completed: true },
+  { id: 1, description: 'Task 1', completed: false },
 ];
 
 @Injectable({
@@ -13,11 +22,21 @@ const INITIAL_TASKS: ITask[] = [
 })
 export class TaskService {
   private tasksSubject = new BehaviorSubject<ITask[]>([]);
+  private errorMessageSubject = new BehaviorSubject<string>('');
+  private snackBar = inject(MatSnackBar);
 
   tasks$ = this.tasksSubject.asObservable();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   fetchTasksFromApi(): Observable<ITask[]> {
-    return of(INITIAL_TASKS).pipe(delay(500)); // simulate an API call
+    return of(INITIAL_TASKS).pipe(
+      first(),
+      delay(500), // simulate an API call
+      catchError((error) => {
+        this.handleError(error);
+        throw error;
+      }),
+    );
   }
 
   setTasks(tasks: ITask[]): void {
@@ -41,5 +60,14 @@ export class TaskService {
       return task;
     });
     this.tasksSubject.next(tasks);
+  }
+
+  handleError(error: HttpErrorResponse) {
+    const { message } = error;
+
+    if (message) {
+      this.errorMessageSubject.next(message);
+      this.snackBar.open(message, '', { duration: 5000 });
+    }
   }
 }
